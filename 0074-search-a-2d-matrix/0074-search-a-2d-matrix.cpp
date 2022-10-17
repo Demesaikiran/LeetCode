185efb56b361b313db84acdb0b093eee898f5898
DemeSaiKiran <demesaikiran@gmail.com>
2022-10-17 07:01:51 +0530

    Time: 4 ms (83.04%), Space: 9.5 MB (78.13%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int rownum = -1;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        cout << m << " " << n << endl;
+        
+        for(int i = 0; i < m; i++)
+        {
+            if(target <= matrix[i][n - 1])
+            {
+                rownum = i;
+                break;
+            }
+        }
+        
+        cout << rownum << endl;
+        if(rownum == -1)
+            return false;
+        
+        if(binary_search(matrix[rownum].begin(), matrix[rownum].end(), target))
+            return true;
+        else
+            return false;
+    }
+};
