e7e7184bcb79d5cc41e0a46863feadd06c922a67
DemeSaiKiran <demesaikiran@gmail.com>
2025-03-06 23:28:20 +0530

    Time: 7 ms (71.22%) | Memory: 29.8 MB (69.08%) - LeetSync

---
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        
+        unordered_map<int, int> hash;
+        int duplicate = 0;
+        int given_sum = 0;
+
+        int n = grid[0].size();
+
+        int n2_sum = ((n* n) * ((n*n) + 1)) /2 ;
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                given_sum += grid[i][j];
+
+                if(duplicate == 0)
+                    if(hash.count(grid[i][j])) 
+                        duplicate = grid[i][j];
+                    else
+                        hash[grid[i][j]] = 1;
+
+            }
+        }
+        cout << given_sum << endl;
+        cout << n2_sum << endl << duplicate << endl;
+        int missing = n2_sum - given_sum + duplicate;
+
+        return {duplicate, missing};
+    }
+};
