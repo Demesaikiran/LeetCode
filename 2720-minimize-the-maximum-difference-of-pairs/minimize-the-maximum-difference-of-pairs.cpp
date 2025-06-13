a8ef6293f526f5d698ce8365797ead7aba2e4541
DemeSaiKiran <demesaikiran@gmail.com>
2025-06-13 23:44:49 +0530

    Time: 35 ms (15.13%) | Memory: 124.4 MB (5.04%) - LeetSync

--- 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.cpp
+++ 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int countValidPairs(vector<int>& nums, int threshold) {
-        int index = 0, count = 0;
-        while (index < nums.size() - 1) {
-            if (nums[index + 1] - nums[index] <= threshold) {
-                count++;
-                index++;
+    int Pairs(vector<int> nums, int p) {
+        int i = 0, cnt = 0;
+        while (i < nums.size() - 1) {
+            if (nums[i + 1] - nums[i] <= p) {
+                cnt++;
+                i++;
             }
-            index++;
+            i++;
         }
-        return count;
+        return cnt;
     }
 
     int minimizeMax(vector<int>& nums, int p) {
         sort(nums.begin(), nums.end());
         int n = nums.size();
         int left = 0, right = nums[n - 1] - nums[0];
-
         while (left < right) {
             int mid = left + (right - left) / 2;
-            if (countValidPairs(nums, mid) >= p)
+            if (Pairs(nums, mid) >= p)
                 right = mid;
             else
                 left = mid + 1;
