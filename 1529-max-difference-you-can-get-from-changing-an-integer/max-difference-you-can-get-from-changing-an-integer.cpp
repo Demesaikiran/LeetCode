23160286565c39032cef95954fc2ea38967c2671
DemeSaiKiran <demesaikiran@gmail.com>
2025-06-15 23:10:33 +0530

    Time: 3 ms (15.17%) | Memory: 8.9 MB (18.48%) - LeetSync

--- 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.cpp
+++ 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.cpp
@@ -5,9 +5,12 @@ public:
         set<char> uniqueDigits(numStr.begin(), numStr.end());
         int maxVal = num, minVal = num;
 
-        for (char digit : uniqueDigits) {
-            for (char newDigit = '0'; newDigit <= '9'; ++newDigit) {
-                if (numStr[0] == digit && newDigit == '0') continue;
+        for (char digit : uniqueDigits) 
+        {
+            for (char newDigit = '0'; newDigit <= '9'; ++newDigit) 
+            {
+                if (numStr[0] == digit && newDigit == '0') 
+                    continue;
                 string newNumStr = numStr;
                 replace(newNumStr.begin(), newNumStr.end(), digit, newDigit);
                 int newNum = stoi(newNumStr);
@@ -15,7 +18,6 @@ public:
                 minVal = min(minVal, newNum);
             }
         }
-
         return maxVal - minVal;
     }
 };
