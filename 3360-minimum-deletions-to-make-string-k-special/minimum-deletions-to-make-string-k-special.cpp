6bbce838257037c6b566ec2de329cd6ff49d6d2d
DemeSaiKiran <demesaikiran@gmail.com>
2025-06-21 22:52:47 +0530

    Time: 3 ms (82.61%) | Memory: 19.3 MB (86.59%) - LeetSync

--- 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.cpp
+++ 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.cpp
@@ -1,7 +1,6 @@
 class Solution {
  public:
-  int minimumDeletions(string word, int k) 
-  {
+  int minimumDeletions(string word, int k) {
     int ans = INT_MAX;
     vector<int> count(26);
 
@@ -11,7 +10,7 @@ class Solution {
     for (const int minFreq : count) {
       int deletions = 0;
       for (const int freq : count)
-        if (freq < minFreq)
+        if (freq < minFreq)  
           deletions += freq;
         else 
           deletions += max(0, freq - (minFreq + k));
